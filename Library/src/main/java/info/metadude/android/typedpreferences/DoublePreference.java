c9337702437125325434c911437c64cca50fdfe6
Tobias Preuss <tobias.preuss@googlemail.com>
2014-03-04 19:57:26 +0100

    Add missing JavaDoc comments to DoublePreference class.

--- Library/src/main/java/info/metadude/android/typedpreferences/DoublePreference.java
+++ Library/src/main/java/info/metadude/android/typedpreferences/DoublePreference.java
@@ -10,26 +10,48 @@ public class DoublePreference implements TypedPreference<Double> {
     protected LongPreference mLongPreference;
     public static final double DEFAULT_VALUE_VALUE = 0d;
 
+    /**
+     * Constructs a {@code double} preference for the given key
+     * having the default value set to zero available.
+     */
     public DoublePreference(SharedPreferences preferences, String key) {
         this(preferences, key, DEFAULT_VALUE_VALUE);
     }
 
+    /**
+     * Constructs a {@code double} preference for the given key
+     * having the default value available.
+     */
     public DoublePreference(SharedPreferences preferences, String key, double defaultValue) {
         mLongPreference = new LongPreference(preferences, key, Double.doubleToLongBits(defaultValue));
     }
 
+    /**
+     * Returns the stored {@code double} value if it exists
+     * or the default value.
+     */
     public Double get() {
         return Double.longBitsToDouble(mLongPreference.get());
     }
 
+    /**
+     * Returns {@code true} if some value is stored for
+     * this preference, otherwise {@code false}.
+     */
     public boolean isSet() {
         return mLongPreference.isSet();
     }
 
+    /**
+     * Stores the given {@code double} value.
+     */
     public void set(Double value) {
         mLongPreference.set(Double.doubleToLongBits(value));
     }
 
+    /**
+     * Removes this preference setting.
+     */
     public void delete() {
         mLongPreference.delete();
     }
