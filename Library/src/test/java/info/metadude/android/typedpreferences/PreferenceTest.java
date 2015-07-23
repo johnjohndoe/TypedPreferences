7030db6cba9c1f259674e18e5f8684f94313088d
Tobias Preuss <tobias.preuss@googlemail.com>
2015-07-23 18:06:27 +0200

    Update Robolectric to v.3.0.

--- Library/src/test/java/info/metadude/android/typedpreferences/PreferenceTest.java
+++ Library/src/test/java/info/metadude/android/typedpreferences/PreferenceTest.java
@@ -3,12 +3,12 @@ package info.metadude.android.typedpreferences;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
-import org.robolectric.Robolectric;
+import org.robolectric.RuntimeEnvironment;
 
 public abstract class PreferenceTest {
 
     public SharedPreferences getSharedPreferences() {
-        return PreferenceManager.getDefaultSharedPreferences(Robolectric.application);
+        return PreferenceManager.getDefaultSharedPreferences(RuntimeEnvironment.application);
     }
 
 }
