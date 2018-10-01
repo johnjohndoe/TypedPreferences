8b4582b2b9d139cd04d0b849fc8a4c62b98d2ace
Tobias Preuss <tobias.preuss@googlemail.com>
2018-10-02 00:02:02 +0200

    Use butterknife v.8.8.1.

--- TypedPreferencesDemo/src/main/java/info/metadude/android/typedpreferences/demo/MainActivity.java
+++ TypedPreferencesDemo/src/main/java/info/metadude/android/typedpreferences/demo/MainActivity.java
@@ -8,16 +8,16 @@ import android.text.TextUtils;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import butterknife.Bind;
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Bind(R.id.storage_current_value)
+    @BindView(R.id.storage_current_value)
     TextView storageValueTextView;
 
-    @Bind(R.id.user_input)
+    @BindView(R.id.user_input)
     EditText userInputEditText;
 
     protected PreferenceHelper mPreferenceHelper;
